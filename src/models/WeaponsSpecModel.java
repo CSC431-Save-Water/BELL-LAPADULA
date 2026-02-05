a3edeaad5422d3c262e843dfddf612eeaf1c3227
jacobdeanpugh <jacobdean.pugh@calbaptist.edu>
2026-02-05 15:07:16 -0800

    WeaponsSpecModel: drop accessors, init fields
    Remove explicit getter and setter methods for name, description, and classification from WeaponsSpecModel and instead initialize DESCRIPTION and CLASSIFICATION fields to null in the constructor (NAME remains initialized). Simplifies the class by relying on the BaseModel field map and removes per-field touch/set/get boilerplate.

--- src/models/WeaponsSpecModel.java
+++ src/models/WeaponsSpecModel.java
@@ -10,33 +10,7 @@ public class WeaponsSpecModel extends BaseModel {
         super(securityLevel);
         this.objectApiName = "Weapon Spec";
         this.fields.put(NAME, name);
-    }
-
-    public String getName(User user) throws Exception {
-        return get(NAME, user);
-    }
-
-    public String getDescription(User user) throws Exception {
-        return get(DESCRIPTION, user);
-    }
-
-    public String getClassification(User user) throws Exception {
-        return get(CLASSIFICATION, user);
-    }
-
-
-    public void setName(String name, User user) throws Exception {
-        set(NAME, name, user);
-        touch();
-    }
-
-    public void setDescription(String description, User user) throws Exception {
-        set(DESCRIPTION, description, user);
-        touch();
-    }
-
-    public void setClassification(String classification, User user) throws Exception {
-        set(CLASSIFICATION, classification, user);
-        touch();
+        this.fields.put(DESCRIPTION, null);
+        this.fields.put(CLASSIFICATION, null);
     }
 }
